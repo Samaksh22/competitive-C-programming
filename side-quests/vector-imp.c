b4968bdea8b12959098242177535aaf64d278a0d
Samaksh22 <samakshsethiya22@gmail.com>
2026-02-01 00:07:18 +0530

    updated the remaining functions in vectors

--- side-quests/vector-imp.c
+++ side-quests/vector-imp.c
@@ -95,7 +95,7 @@ typedef struct {
 } vec_int;
 
 // --- UTILS ---
-size_t next_pow_2(size_t n) {
+size_t _next_pow_2(size_t n) {
     if (n == 0) {
         return 0;
     }
@@ -118,7 +118,7 @@ vec_int vec_init() {
 }
 
 vec_int vec_with_cap(size_t cap) {
-    cap = next_pow_2(cap);
+    cap = _next_pow_2(cap);
     if (cap == 0) {
         return vec_init();
     }
@@ -175,23 +175,65 @@ int vec_back(vec_int v) {
     return v.arr[v.len - 1];
 }
 
+// --- MISC 1 ---
+
+int vec_reserve(vec_int *v, size_t n) {
+    if (n <= v->cap) {
+        return 1;
+    }
+
+    size_t new_cap = _next_pow_2(n);
+    if (new_cap == 0) {
+        return 0;
+    }
+
+    int *ptr = realloc(v->arr, new_cap * sizeof(int));
+    if (ptr == NULL) {
+        return 0;
+    }
+
+    v->arr = ptr;
+    v->cap = new_cap;
+    return 1;
+}
+
+int vec_shrink(vec_int *v) {
+    if (v->len == 0) {
+        vec_drop(v);
+        return 1;
+    }
+
+    size_t new_cap = _next_pow_2(v->len);
+    if (new_cap == 0) {
+        return 0;
+    }
+
+    int *ptr = realloc(v->arr, new_cap * sizeof(int));
+    if (ptr == NULL) {
+        return 0;
+    }
+
+    v->arr = ptr;
+    v->cap = new_cap;
+    return 1;
+}
+
+vec_int vec_copy(vec_int v) {
+    vec_int new_v = {.arr = NULL, .len = v.len, .cap = v.cap};
+    new_v.arr = malloc(new_v.cap * sizeof(int));
+    if (new_v.arr == NULL) {
+        return vec_init();
+    }
+
+    memcpy(new_v.arr, v.arr, new_v.len * sizeof(int));
+    return new_v;
+}
+
 // --- MUTATIONS ---
 
 int vec_push(vec_int *v, int x) {
-    if (v->len == v->cap) {
-        size_t new_cap;
-        if (v->cap == 0) {
-            new_cap = 1;
-        } else {
-            new_cap = v->cap * 2;
-        }
-
-        int *ptr = realloc(v->arr, new_cap * sizeof(int));
-        if (ptr == NULL) {
-            return 0;
-        }
-        v->cap = new_cap;
-        v->arr = ptr;
+    if (vec_reserve(v, v->len + 1) == 0) {
+        return 0;
     }
     v->arr[v->len] = x;
     v->len++;
@@ -204,22 +246,9 @@ int vec_pop(vec_int *v) {
 }
 
 int vec_insert(vec_int *v, size_t i, int x) {
-    if (v->len == v->cap) {
-        size_t new_cap;
-        if (v->cap == 0) {
-            new_cap = 1;
-        } else {
-            new_cap = v->cap * 2;
-        }
-
-        int *ptr = realloc(v->arr, new_cap * sizeof(int));
-        if (ptr == NULL) {
-            return 0;
-        }
-        v->cap = new_cap;
-        v->arr = ptr;
+    if (vec_reserve(v, v->len + 1) == 0) {
+        return 0;
     }
-
     for (size_t j = v->len; j > i; j--) {
         v->arr[j] = v->arr[j - 1];
     }
@@ -231,11 +260,40 @@ int vec_insert(vec_int *v, size_t i, int x) {
 int vec_erase(vec_int *v, size_t i) {
     int x = v->arr[i];
     for (; i + 1 < v->len; i++) {
-        v->arr[i] = v->arr[i+1];
+        v->arr[i] = v->arr[i + 1];
     }
     v->len--;
 
     return x;
+}
+
+// --- MISC ---
+
+size_t vec_find(vec_int v, int x) {
+    size_t idx = 0;
+    for (; idx < v.len; idx++) {
+        if (v.arr[idx] == x) {
+            break;
+        }
+    }
+
+    return idx;
+}
+
+int _asc_comp(const void *a, const void *b) {
+    return (*(int *)a - *(int *)b);
+}
+
+int _desc_comp(const void *a, const void *b) {
+    return (*(int *)b - *(int *)a);
+}
+
+void vec_sort(vec_int *v, int ascending) {
+    if (ascending) {
+        qsort((v->arr), v->len, sizeof(int), _asc_comp);
+    } else {
+        qsort((v->arr), v->len, sizeof(int), _desc_comp);
+    }
 }
 
 int main() {
